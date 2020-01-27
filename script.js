3e09b92d987c46d6226b9b49eac005454ba709d3
themanmaran <tyler.maran@gmail.com>
2020-01-27 11:25:01 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let submit = document.getElementById('add_url');
+let iframe = document.getElementById('iphone');
+let url_input = document.getElementById('url_input');
+
+console.log(iframe);
+console.log(submit);
+
+const update_frame = (url) => {
+    iframe.src = url;
+}
+
+
+submit.onclick = () => update_frame(url_input.value);
+
