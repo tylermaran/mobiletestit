1e2ff225b3d8e688ef1f16de7a4c333e4a98c4d4
themanmaran <tyler.maran@gmail.com>
2020-01-27 17:14:42 -0800

    more phone styling and dimensions

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let url_input = document.getElementById('url_input');
 let phone_time = document.getElementById('phone_time');
 let phone_address = document.getElementById('phone_address');
 
-console.log(iframe);
+// console.log(iframe);
 console.log(submit);
 
 const update_frame = (url) => {
@@ -12,6 +12,21 @@ const update_frame = (url) => {
 
     iframe.src = 'https://' + url;
     phone_address.innerHTML = url;
+
+    setTimeout(() => {
+        console.log(iframe.scrollWidth);
+
+    }, 100);
+
+
+
+    let frame_width = iframe.scrollWidth;
+    console.log('iframe width = ' + frame_width);
+    if (frame_width - 20 > 375) {
+        console.log('Too wide');
+    }
+
+
 }
 
 
