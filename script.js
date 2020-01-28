f38ad30bb438db0320dce2c2a16d021ee2a95b4a
themanmaran <tyler.maran@gmail.com>
2020-01-27 16:33:51 -0800

    changing out the address bar dynamically

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let submit = document.getElementById('add_url');
 let iframe = document.getElementById('phone');
 let url_input = document.getElementById('url_input');
+let phone_time = document.getElementById('phone_time');
+let phone_address = document.getElementById('phone_address');
 
 console.log(iframe);
 console.log(submit);
 
 const update_frame = (url) => {
-    iframe.src = url;
+    url = url.split('//')[1];
+
+    iframe.src = 'https://' + url;
+    phone_address.innerHTML = url;
 }
 
 
